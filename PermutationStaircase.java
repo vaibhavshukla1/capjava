a57b9e3556af9b419d24b1d6dc7312839bbbba48
vaibhavshukla1 <35499885+vaibhavshukla1@users.noreply.github.com>
2019-01-23 16:43:30 +0530

    Add files via upload

---
+++ PermutationStaircase.java
@@ -0,0 +1,20 @@
+package program1;
+
+import java.util.Scanner;
+
+public class PermutationStaircase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			count+=i;
+		}
+		System.out.println(count+1);
+
+	}
+
+}
